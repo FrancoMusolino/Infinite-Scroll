768861d58adababab35d049c53d6f1b3d47752a3
FrancoMusolino <francomusolino55@gmail.com>
2022-05-05 18:31:12 -0300

    Styling & first steps of handling pages

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,21 +1,42 @@
+import React, { useState } from "react";
 import axios from "axios";
-import React from "react";
 import { useInfiniteQuery } from "react-query";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 const fetchUsers = ({ pageParam = 1 }) =>
   axios.get(`https://reqres.in/api/users?page=${pageParam}`);
 
 const UsersList = () => {
-  const { data, error, isError, isLoading, hasNextPage, fetchNextPage } =
-    useInfiniteQuery(["users"], fetchUsers, {
-      getNextPageParam: (lastPage, pages) => {
-        if (pages.length < 2) {
-          return pages.length + 1;
-        } else {
-          return undefined;
-        }
-      },
-    });
+  const {
+    data,
+    error,
+    isError,
+    isLoading,
+    hasNextPage,
+    hasPreviousPage,
+    fetchNextPage,
+    fetchPreviousPage,
+  } = useInfiniteQuery(["users"], fetchUsers, {
+    getNextPageParam: (lastPage, pages) => {
+      if (pages.length < 2) {
+        return pages.length + 1;
+      } else {
+        return undefined;
+      }
+    },
+    getPreviousPageParam: (firstPage, pages) => {
+      if (pages.length > 1) {
+        return pages.length - 1;
+      } else {
+        return undefined;
+      }
+    },
+  });
 
   if (isError) {
     return <h2>Ha ocurrido un error: {error}</h2>;
@@ -28,19 +49,68 @@ const UsersList = () => {
   return (
     <>
       <div>
-        {data?.pages.map((group, i) => {
-          return (
-            <div key={i}>
-              {group.data.data.map((user) => (
-                <h1 key={user.id}>{user.first_name}</h1>
-              ))}
-            </div>
-          );
-        })}
+        {data?.pages.map((group, i) => (
+          <div key={i}>
+            {group.data.data.map((user) => (
+              <div
+                style={{ width: "100%", maxWidth: 360, margin: "0 auto" }}
+                key={user.id}
+              >
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar alt={user.first_name} src={user.avatar} />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={`${user.first_name} ${user.last_name}`}
+                    secondary={
+                      <React.Fragment>
+                        <Typography
+                          sx={{ display: "inline" }}
+                          component="span"
+                          variant="body2"
+                          color="text.primary"
+                        >
+                          {user.email}
+                        </Typography>
+                      </React.Fragment>
+                    }
+                  />
+                </ListItem>
+                <hr></hr>
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
-      <button disabled={!hasNextPage} onClick={() => fetchNextPage()}>
-        Load More
-      </button>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          margin: "0 auto",
+          marginTop: "50px",
+          width: "50%",
+        }}
+      >
+        <Button
+          disabled={!hasPreviousPage}
+          onClick={() => fetchPreviousPage()}
+          variant="contained"
+          component="span"
+          sx={{ width: "max-width" }}
+        >
+          1
+        </Button>
+
+        <Button
+          disabled={!hasNextPage}
+          onClick={() => fetchNextPage()}
+          variant="contained"
+          component="span"
+          sx={{ width: "min-width" }}
+        >
+          2
+        </Button>
+      </div>
     </>
   );
 };
